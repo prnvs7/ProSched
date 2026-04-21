a0fe3d11289ce060bf2308c75d99a89a0ee0d5ce
Pranav SIngh <pranavsingh00710@gmail.com>
2026-04-21 10:39:05 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/factory/",
+  base: "/ProSched/",
   server: {
     host: "::",
     port: 8080,
